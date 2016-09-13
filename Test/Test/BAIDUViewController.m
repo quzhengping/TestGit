34a8a08447449693427f976ec83f6c21f50965c4
曲正平 <zhengping@quzhengpingdeMacBook-Pro.local>
2016-09-13 16:48:55 +0800

    Baidu

---
+++ Test/Test/BAIDUViewController.m
@@ -0,0 +1,37 @@
+//
+//  BAIDUViewController.m
+//  Test
+//
+//  Created by 曲 on 16/9/13.
+//  Copyright © 2016年 曲正平. All rights reserved.
+//
+
+#import "BAIDUViewController.h"
+
+@interface BAIDUViewController ()
+
+@end
+
+@implementation BAIDUViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
